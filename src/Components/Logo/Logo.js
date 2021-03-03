8dbb3ee85a54223ea45a36ea9e21c5313abcdcc9
Your Name <you@example.com>
2021-03-04 02:30:39 +0900

    adding basic components

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+
+const Logout = () => {
+	return(
+		<div className='ma4 mt0'>
+			<Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+ 				<div className="Tilt-inner"> 👽 </div>
+			</Tilt>	
+		</div>
+	);
+}
+
+
+
+
+
+
+export default Logo; //DON'T FORGET TO DO THIS
